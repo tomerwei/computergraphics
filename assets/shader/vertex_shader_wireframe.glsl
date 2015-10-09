cc54208024870e3ee1e608656ca88b3dfa4a1dfb
Philipp Jenke <philipp.jenke@haw-hamburg.de>
2015-10-09 16:51:39 +0200

    Documentation and Wireframe Shader

--- assets/shader/vertex_shader_wireframe.glsl
+++ assets/shader/vertex_shader_wireframe.glsl
@@ -4,4 +4,6 @@ uniform mat4 proj, view;
 void main(){
     vBC = barycentric;
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    float offset = - 0.001;
+    gl_Position.z = gl_Position.z + offset;
 }
