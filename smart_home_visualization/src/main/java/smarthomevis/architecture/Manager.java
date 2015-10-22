3d5803669ae9e611ca618f064dad759f6f13e2d9
Philipp Jenke <philipp.jenke@haw-hamburg.de>
2015-10-22 07:16:36 +0200

    Fixed DataSource name

--- smart_home_visualization/src/main/java/smarthomevis/architecture/Manager.java
+++ smart_home_visualization/src/main/java/smarthomevis/architecture/Manager.java
@@ -4,38 +4,38 @@ import cgresearch.graphics.bricks.CgApplication;
 import com.mongodb.MongoClient;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
-import smarthomevis.architecture.objects.Datasource;
+import smarthomevis.architecture.objects.DataSource;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Manager extends CgApplication {
 
-    private List<String> datasources;
-    private List<String> groups;
+  private List<String> datasources;
+  private List<String> groups;
 
-    Datastore datastore;
+  Datastore datastore;
 
-    public Manager() {
-        datasources = new ArrayList<>();
-        groups = new ArrayList<>();
-    }
+  public Manager() {
+    datasources = new ArrayList<>();
+    groups = new ArrayList<>();
+  }
 
-    public void connectToMongoDB(String databaseName) {
-        MongoClient mongoClient = new MongoClient();
-        //MongoDatabase mongoDB = mongoClient.getDatabase(databaseName);
+  public void connectToMongoDB(String databaseName) {
+    MongoClient mongoClient = new MongoClient();
+    // MongoDatabase mongoDB = mongoClient.getDatabase(databaseName);
 
-        final Morphia morphia = new Morphia();
-        morphia.mapPackage("cg.smart_home_visualization.src.main.java.smarthomevis.architecture");
-        datastore = morphia.createDatastore(mongoClient, databaseName);
-        datastore.ensureIndexes();
-    }
+    final Morphia morphia = new Morphia();
+    morphia.mapPackage("cg.smart_home_visualization.src.main.java.smarthomevis.architecture");
+    datastore = morphia.createDatastore(mongoClient, databaseName);
+    datastore.ensureIndexes();
+  }
 
-    public void addDatasource() {
-        Datasource datasource = new Datasource("myId1");
-        datasource.addData("1999-12-31");
+  public void addDatasource() {
+    DataSource datasource = new DataSource("myId1");
+    datasource.addData("1999-12-31");
 
-        datastore.save(datasource);
-    }
+    datastore.save(datasource);
+  }
 
 }
--- smart_home_visualization/src/main/java/smarthomevis/architecture/objects/DataSource.java
+++ smart_home_visualization/src/main/java/smarthomevis/architecture/objects/DataSource.java
@@ -3,28 +3,26 @@ package smarthomevis.architecture.objects;
 import org.mongodb.morphia.annotations.*;
 
 @Entity("datasources")
-@Indexes(
-        @Index(value = "date", fields = @Field("date"))
-)
-public class Datasource {
+@Indexes(@Index(value = "date", fields = @Field("date") ))
+public class DataSource {
 
-    @Id
-    private String id;
-    private String date;
+  @Id
+  private String id;
+  private String date;
 
-    public Datasource(String id) {
-        this.id = id;
-    }
+  public DataSource(String id) {
+    this.id = id;
+  }
 
-    public void addData(String date) {
-        this.date += date;
-    }
+  public void addData(String date) {
+    this.date += date;
+  }
 
-    public String getDate() {
-        return date;
-    }
+  public String getDate() {
+    return date;
+  }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+  public void setDate(String date) {
+    this.date = date;
+  }
 }
