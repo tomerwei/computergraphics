ed5c58191e796f17c76f943e817f283e49cc2c7f
Vitos <Vitos@Vitos-PC.fritz.box>
2016-01-24 16:48:10 +0100

    Butterfly

---
+++ apps/src/main/java/cgresearch/studentprojects/autogenerator/Body.java
@@ -0,0 +1,250 @@
+package cgresearch.studentprojects.autogenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cgresearch.core.math.IVector;
+import cgresearch.core.math.IVector3;
+import cgresearch.core.math.VectorMatrixFactory;
+import cgresearch.graphics.datastructures.curves.BezierCurve;
+import cgresearch.graphics.datastructures.primitives.Line3D;
+
+public class Body {
+
+	private IVector3 center;
+
+	private IVector3 A;
+	private IVector3 B;
+	private IVector3 C;
+	private IVector3 D;
+
+	private Line3D AB;
+	private Line3D BC;
+	private Line3D CD;
+	private Line3D DA;
+
+	private BezierCurve topLeft;
+	private BezierCurve topRight;
+	private BezierCurve bottomLeft;
+	private BezierCurve bottomRight;
+
+	private double hoehe;
+	private double laenge;
+
+	private Line3D[] lines = new Line3D[4];
+	private List<BezierCurve> curves = new ArrayList<BezierCurve>();
+
+	public Body(IVector3 center, double hoehe, double laenge, IVector3 tl, IVector3 tr, double wingMitteBreite) {
+		setCenter(center);
+		setHoehe(hoehe);
+		setLaenge(laenge);
+		setA(center, hoehe, laenge);
+		setB(center, hoehe, laenge);
+		setC(center, hoehe, laenge);
+		setD(center, hoehe, laenge);
+		setAB();
+		setBC();
+		setCD();
+		setDA();
+		setLines();
+		setTopLeft(tl, wingMitteBreite);
+		setBottomLeft(tl, wingMitteBreite);
+		setTopRight(tr, wingMitteBreite);
+		setBottomRight(tr, wingMitteBreite);
+		setCurves(topLeft, topRight, bottomLeft, bottomRight);
+	}
+
+	public IVector3 getCenter() {
+		return center;
+	}
+
+	public void setCenter(IVector3 center) {
+		this.center = center;
+	}
+
+	public IVector3 getA() {
+		return A;
+	}
+
+	public void setA(IVector3 center, double hoehe, double laenge) {
+		double x = center.get(0) - laenge / 2;
+		double y = center.get(1) - hoehe / 2;
+		A = VectorMatrixFactory.newIVector3(x, y, 0);
+	}
+
+	public IVector3 getB() {
+		return B;
+	}
+
+	public void setB(IVector3 center, double hoehe, double laenge) {
+		double x = center.get(0) - laenge / 2;
+		double y = center.get(1) + hoehe / 2;
+		B = VectorMatrixFactory.newIVector3(x, y, 0);
+	}
+
+	public IVector3 getC() {
+		return C;
+	}
+
+	public void setC(IVector3 center, double hoehe, double laenge) {
+		double x = center.get(0) + laenge / 2;
+		double y = center.get(1) + hoehe / 2;
+		C = VectorMatrixFactory.newIVector3(x, y, 0);
+	}
+
+	public IVector3 getD() {
+		return D;
+	}
+
+	public void setD(IVector3 center, double hoehe, double laenge) {
+		double x = center.get(0) + laenge / 2;
+		double y = center.get(1) - hoehe / 2;
+		D = VectorMatrixFactory.newIVector3(x, y, 0);
+	}
+
+	public Line3D getAB() {
+		return AB;
+	}
+
+	public void setAB() {
+		AB = new Line3D(this.A, this.B);
+	}
+
+	public Line3D getBC() {
+		return BC;
+	}
+
+	public void setBC() {
+		BC = new Line3D(this.B, this.C);
+	}
+
+	public Line3D getCD() {
+		return CD;
+	}
+
+	public void setCD() {
+		CD = new Line3D(this.C, this.D);
+	}
+
+	public Line3D getDA() {
+		return DA;
+	}
+
+	public void setDA() {
+		DA = new Line3D(this.D, this.A);
+	}
+
+	public double getHoehe() {
+		return hoehe;
+	}
+
+	public void setHoehe(double hoehe) {
+		this.hoehe = hoehe;
+	}
+
+	public double getLaenge() {
+		return laenge;
+	}
+
+	public void setLaenge(double laenge) {
+		this.laenge = laenge;
+	}
+
+	public Line3D[] getLines() {
+		return lines;
+	}
+
+	public void setLines() {
+		this.lines[0] = AB;
+		this.lines[1] = BC;
+		this.lines[2] = CD;
+		this.lines[3] = DA;
+	}
+
+	public BezierCurve getTopLeft() {
+		return topLeft;
+	}
+
+	public void setTopLeft(IVector3 tl, double wingMitteBreite) {
+
+		double breite = wingMitteBreite / 2;
+
+		BezierCurve curve = new BezierCurve(2);
+
+		curve.setControlPoint(0, VectorMatrixFactory.newIVector3(tl.get(0), tl.get(1) + breite, 0));
+
+		curve.setControlPoint(1, VectorMatrixFactory.newIVector3(B.get(0), B.get(1), 0));
+
+		curve.setControlPoint(2, VectorMatrixFactory.newIVector3(B.get(0) + laenge / 2, B.get(1), 0));
+
+		this.topLeft = curve;
+	}
+
+	public BezierCurve getTopRight() {
+		return topRight;
+	}
+
+	public void setTopRight(IVector3 tr, double wingMitteBreite) {
+
+		double breite = wingMitteBreite / 2;
+
+		BezierCurve curve = new BezierCurve(2);
+
+		curve.setControlPoint(0, VectorMatrixFactory.newIVector3(B.get(0) + laenge / 2, B.get(1), 0));
+
+		curve.setControlPoint(1, VectorMatrixFactory.newIVector3(C.get(0), C.get(1), 0));
+
+		curve.setControlPoint(2, VectorMatrixFactory.newIVector3(tr.get(0), tr.get(1) + breite, 0));
+
+		this.topRight = curve;
+	}
+
+	public BezierCurve getBottomLeft() {
+		return bottomLeft;
+	}
+
+	public void setBottomLeft(IVector3 bl, double wingMitteBreite) {
+
+		double breite = wingMitteBreite / 2;
+
+		BezierCurve curve = new BezierCurve(2);
+
+		curve.setControlPoint(0, VectorMatrixFactory.newIVector3(A.get(0) + laenge / 2, A.get(1), 0));
+
+		curve.setControlPoint(1, VectorMatrixFactory.newIVector3(A.get(0), A.get(1), 0));
+
+		curve.setControlPoint(2, VectorMatrixFactory.newIVector3(bl.get(0), bl.get(1) - breite, 0));
+
+		this.bottomLeft = curve;
+	}
+
+	public BezierCurve getBottomRight() {
+		return bottomRight;
+	}
+
+	public void setBottomRight(IVector3 br, double wingMitteBreite) {
+
+		double breite = wingMitteBreite / 2;
+
+		BezierCurve curve = new BezierCurve(2);
+
+		curve.setControlPoint(0, VectorMatrixFactory.newIVector3(br.get(0), br.get(1) - breite, 0));
+
+		curve.setControlPoint(1, VectorMatrixFactory.newIVector3(D.get(0), D.get(1), 0));
+
+		curve.setControlPoint(2, VectorMatrixFactory.newIVector3(A.get(0) + laenge / 2, A.get(1), 0));
+
+		this.bottomRight = curve;
+	}
+
+	public List<BezierCurve> getCurves() {
+		return curves;
+	}
+
+	public void setCurves(BezierCurve... curves) {
+		for (BezierCurve c : curves) {
+			this.curves.add(c);
+		}
+	}
+
+}
--- apps/src/main/java/cgresearch/studentprojects/autogenerator/Generator.java
+++ apps/src/main/java/cgresearch/studentprojects/autogenerator/Generator.java
@@ -8,29 +8,30 @@ import cgresearch.graphics.bricks.CgApplication;
 
 public class Generator extends CgApplication {
 
-  /**
-   * Constructor
-   */
+	/**
+	 * Constructor
+	 */
 
-  public Generator() {
+	public Generator() {
 
-  }
+	}
 
-  /**
-   * Program entry point.
-   */
-  public static void main(String[] args) {
+	/**
+	 * Program entry point.
+	 */
+	public static void main(String[] args) {
 
-    ResourcesLocator.getInstance().parseIniFile("resources.ini");
-    Generator frame = new Generator();
-    JoglAppLauncher appLauncher = JoglAppLauncher.getInstance();
-    appLauncher.create(frame);
-    appLauncher.setRenderSystem(RenderSystem.JOGL);
-    appLauncher.setUiSystem(UI.JOGL_SWING);
-    // GeneratorGUI gui = new GeneratorGUI(frame.getCgRootNode());
-//    GeneratorGUI2D gui = new GeneratorGUI2D(frame.getCgRootNode());
-    GeneratorGUI2DNew gui = new GeneratorGUI2DNew(frame.getCgRootNode());
-    appLauncher.addCustomUi(gui);
+		ResourcesLocator.getInstance().parseIniFile("resources.ini");
+		Generator frame = new Generator();
+		JoglAppLauncher appLauncher = JoglAppLauncher.getInstance();
+		appLauncher.create(frame);
+		appLauncher.setRenderSystem(RenderSystem.JOGL);
+		appLauncher.setUiSystem(UI.JOGL_SWING);
+		// GeneratorGUI gui = new GeneratorGUI(frame.getCgRootNode());
+		// GeneratorGUI2D gui = new GeneratorGUI2D(frame.getCgRootNode());
+		// GeneratorGUI2DNew gui = new GeneratorGUI2DNew(frame.getCgRootNode());
+		GeneratorGUIBut gui = new GeneratorGUIBut(frame.getCgRootNode());
+		appLauncher.addCustomUi(gui);
 
-  }
+	}
 }
