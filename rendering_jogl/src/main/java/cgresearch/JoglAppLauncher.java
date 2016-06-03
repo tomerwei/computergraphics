c2508cffb64dfcd4a81dac28bde9d60d4ef5a176
Lars Porep <larshendrik.porep@haw-hamburg.de>
2016-06-03 13:39:31 +0200

    Getter JoglFrame

--- rendering_jogl/src/main/java/cgresearch/JoglAppLauncher.java
+++ rendering_jogl/src/main/java/cgresearch/JoglAppLauncher.java
@@ -1,7 +1,8 @@
 package cgresearch;
 
-import cgresearch.AppLauncher;
 import cgresearch.core.logging.Logger;
+import cgresearch.rendering.core.IRenderObjectsFactory;
+import cgresearch.rendering.jogl.core.JoglRenderNode;
 import cgresearch.rendering.jogl.core.JoglRenderable;
 import cgresearch.rendering.jogl.ui.JoglFrame;
 import cgresearch.rendering.jogl.ui.JoglSwingUserInterface;
@@ -14,67 +15,104 @@ import cgresearch.rendering.jogl.ui.JoglSwingUserInterface;
  */
 public class JoglAppLauncher extends AppLauncher {
 
-  /**
-   * Constructor.
-   */
-  private JoglAppLauncher() {
-    super();
-  }
+	/**
+	 * Constructor.
+	 */
+	private JoglAppLauncher() {
+		super();
+	}
 
-  /**
-   * Get singleton instance
-   */
-  public static JoglAppLauncher getInstance() {
-    if (instance == null) {
-      instance = new JoglAppLauncher();
-    }
-    return (JoglAppLauncher) instance;
-  }
+	/**
+	 * Register a custom RenderObjectFactory. setRenderSystem(RenderSystem.JOGL)
+	 * has to be called first!
+	 * 
+	 * ATTENTION: Currently only RenderSystem.JOGL is supported.
+	 * 
+	 * To fix: Accept different types for T in IRenderObjectsFactory
+	 * <T> (Currently only JoglRenderNode is accepted) and forward to
+	 * renderSystem.registerRenderObjectsFactory(factory)
+	 * 
+	 * @param factory
+	 *            The RenderObjectFactory to be added to the rendersystem.
+	 */
+	public void registerRenderObjectsFactory(IRenderObjectsFactory<JoglRenderNode> factory) {
+		if (null == renderSystem) {
+			Logger.getInstance().error("Render system has to be set first! - Use setRenderSystem(RenderSystem rS)");
+			return;
+		}
+		if (renderSystem instanceof JoglFrame) {
+			((JoglFrame) renderSystem).registerRenderObjectsFactory(factory);
+		} else {
+			Logger.getInstance().error("Currently only RenderSystem.JOGL supported.");
+		}
+	}
 
-  /**
-   * Set rendering system.
-   */
-  public void setRenderSystem(RenderSystem rS) {
-    if (this.renderSystem != null) {
-      Logger.getInstance().error("Render system should only be set once!");
-      return;
-    }
+	/**
+	 * Get singleton instance
+	 */
+	public static JoglAppLauncher getInstance() {
+		if (instance == null) {
+			instance = new JoglAppLauncher();
+		}
+		return (JoglAppLauncher) instance;
+	}
 
-    if (rS == RenderSystem.JOGL) {
-      renderSystem = new JoglFrame(app);
-    } else {
-      super.setRenderSystem(rS);
-    }
-  }
+	/**
+	 * Set rendering system.
+	 */
+	public void setRenderSystem(RenderSystem rS) {
+		if (this.renderSystem != null) {
+			Logger.getInstance().error("Render system should only be set once!");
+			return;
+		}
 
-  /**
-   * Set UI system.
-   */
-  public void setUiSystem(UI ui) {
-    if (this.uiSystem != null) {
-      Logger.getInstance().error("UI system should only be set once!");
-      return;
-    }
+		if (rS == RenderSystem.JOGL) {
+			renderSystem = new JoglFrame(app);
+		} else {
+			super.setRenderSystem(rS);
+		}
+	}
 
-    if (ui == UI.JOGL_SWING) {
-      if (renderSystem instanceof JoglFrame) {
-        uiSystem = new JoglSwingUserInterface(app, (JoglFrame) renderSystem);
-      } else {
-        Logger.getInstance().message("Cannot create Jogl-Swing-UI without valid JoglFrame.");
-      }
-    } else {
-      super.setUiSystem(ui);
-    }
-  }
+	/**
+	 * Set UI system.
+	 */
+	public void setUiSystem(UI ui) {
+		if (this.uiSystem != null) {
+			Logger.getInstance().error("UI system should only be set once!");
+			return;
+		}
 
-  /**
-   * Add a renderable to the render system if possible.
-   */
-  public void addRenderable(JoglRenderable renderable) {
-    if (renderSystem instanceof JoglFrame) {
-      ((JoglFrame) renderSystem).addRenderable(renderable);
-    } else {
-      Logger.getInstance().message("Cannot add renderable to current render system.");
-    }
-  }
+		if (ui == UI.JOGL_SWING) {
+			if (renderSystem instanceof JoglFrame) {
+				uiSystem = new JoglSwingUserInterface(app, (JoglFrame) renderSystem);
+			} else {
+				Logger.getInstance().message("Cannot create Jogl-Swing-UI without valid JoglFrame.");
+			}
+		} else {
+			super.setUiSystem(ui);
+		}
+	}
+
+	/**
+	 * Add a renderable to the render system if possible.
+	 */
+	public void addRenderable(JoglRenderable renderable) {
+		if (renderSystem instanceof JoglFrame) {
+			((JoglFrame) renderSystem).addRenderable(renderable);
+		} else {
+			Logger.getInstance().message("Cannot add renderable to current render system.");
+		}
+	}
+
+	/**
+	 * Getter for the current JoglFrame
+	 * Only possible if current renderSystem is set to UI.JOGL_SWING
+	 * @return renderSystem:JoglFrame
+	 */
+	public JoglFrame getJoglFrame() {
+		if (!(renderSystem instanceof JoglFrame)) {
+			Logger.getInstance().error("renderSystem != JoglFrame. Can not return System");
+		}
+		return (JoglFrame) renderSystem;
+	}
 }
