8235a23f2118a34a4f577a1723560fc94869882e
Lars Porep <larshendrik.porep@haw-hamburg.de>
2016-07-26 13:45:23 +0200

    Added correct bone TriMesh creation. Taking rotation of bones into account

--- apps/src/main/java/cgresearch/studentprojects/posegen/datastructure/Bone.java
+++ apps/src/main/java/cgresearch/studentprojects/posegen/datastructure/Bone.java
@@ -113,33 +113,130 @@ public class Bone extends TriangleMesh {
 
 	private void updateMesh() {
 		this.clear();
-		Vector v1 = new Vector(this.startBonePosition.get(0) + 0.05, this.startBonePosition.get(1) + 0.05,
-				this.startBonePosition.get(2) - 0.05);
 
-		Vector v2 = new Vector(this.startBonePosition.get(0) - 0.05, this.startBonePosition.get(1) - 0.05,
-				this.startBonePosition.get(2) - 0.05);
+		// At 0Deg
+		Vector addV1 = new Vector(0.04, 0.04, -0.04); //Basis abstand vom start
+		Vector addV2 = new Vector(0.04, -0.04, -0.04); //Basierend auf 0Degree rotation
+		Vector addV3 = new Vector(0.04, 0.04, 0.04);
+		Vector addV4 = new Vector(0.04, -0.04, 0.04);
 
-		Vector v3 = new Vector(this.startBonePosition.get(0) - 0.05, this.startBonePosition.get(1),
-				this.startBonePosition.get(2) + 0.05);
-		Vector vtop = new Vector(this.endBonePosition.get(0), this.endBonePosition.get(1), this.endBonePosition.get(2));
+		double rotation = getBoneRotation(startBonePosition, endBonePosition);
+		rotation = Math.toDegrees(rotation);
+		if (startBonePosition.get(1) > endBonePosition.get(1)) {
+			rotation = rotation * -1.0;
+		}
+
+		Vector nullVector = new Vector(0, 0, 0);
+		addV1 = rotatePositionByDegree(rotation, addV1, nullVector);
+		addV2 = rotatePositionByDegree(rotation, addV2, nullVector);
+		addV3 = rotatePositionByDegree(rotation, addV3, nullVector);
+		addV4 = rotatePositionByDegree(rotation, addV4, nullVector);
+
+		Vector v1 = startBonePosition.add(addV1);
+		Vector v2 = startBonePosition.add(addV2);
+		Vector v3 = startBonePosition.add(addV3);
+		Vector v4 = startBonePosition.add(addV4);
+
+		// Vector v2 = new Vector(this.startBonePosition.get(0) - 0.05,
+		// this.startBonePosition.get(1) - 0.05, this.startBonePosition.get(2) -
+		// 0.05);
+
+		// Vector v3 = new Vector(this.startBonePosition.get(0) + 0.05,
+		// this.startBonePosition.get(1) - 0.05, this.startBonePosition.get(2) +
+		// 0.05);
+		// Vector v4 = new Vector(this.startBonePosition.get(0) - 0.05,
+		// this.startBonePosition.get(1) - 0.05, this.startBonePosition.get(2) +
+		// 0.05);
+		Vector vTop = new Vector(this.startBonePosition.get(0), this.startBonePosition.get(1),
+				this.startBonePosition.get(2));
+		Vector vEnd = new Vector(this.endBonePosition.get(0), this.endBonePosition.get(1), this.endBonePosition.get(2));
 
 		IVertex vertex1 = new Vertex(VectorFactory.createVector3(v1.get(0), v1.get(1), v1.get(2)));
 		IVertex vertex2 = new Vertex(VectorFactory.createVector3(v2.get(0), v2.get(1), v2.get(2)));
 		IVertex vertex3 = new Vertex(VectorFactory.createVector3(v3.get(0), v3.get(1), v3.get(2)));
-		IVertex vertexTop = new Vertex(VectorFactory.createVector3(vtop.get(0), vtop.get(1), vtop.get(2)));
+		IVertex vertex4 = new Vertex(VectorFactory.createVector3(v4.get(0), v4.get(1), v4.get(2)));
+		IVertex vertexTop = new Vertex(VectorFactory.createVector3(vTop.get(0), vTop.get(1), vTop.get(2)));
+		IVertex vertexEnd = new Vertex(VectorFactory.createVector3(vEnd.get(0), vEnd.get(1), vEnd.get(2)));
 
 		this.addVertex(vertexTop);
 		this.addVertex(vertex1);
 		this.addVertex(vertex2);
 		this.addVertex(vertex3);
+		this.addVertex(vertex4);
+		this.addVertex(vertexEnd);
 
-		this.addTriangle(0, 1, 2);
-		this.addTriangle(0, 2, 3);
-		this.addTriangle(0, 1, 3);
-		this.addTriangle(1, 2, 3);
+		this.addTriangle(0, 1, 2); // Top, left, right : front
+		this.addTriangle(0, 1, 3); // Top left left front/back
+		this.addTriangle(0, 3, 4); // Top left right back
+		this.addTriangle(0, 4, 1); // Top right back/front
+
+		this.addTriangle(5, 1, 2);
+		this.addTriangle(5, 1, 3);
+		this.addTriangle(5, 3, 4);
+		this.addTriangle(5, 4, 1);
 
 		// setColorNotSelected();
 		this.updateRenderStructures();
+
+	}
+
+	// private void updateMesh() {
+	// this.clear();
+	// Vector v1 = new Vector(this.startBonePosition.get(0) + 0.05,
+	// this.startBonePosition.get(1) + 0.05,
+	// this.startBonePosition.get(2) - 0.05);
+	//
+	// Vector v2 = new Vector(this.startBonePosition.get(0) - 0.05,
+	// this.startBonePosition.get(1) - 0.05,
+	// this.startBonePosition.get(2) - 0.05);
+	//
+	// Vector v3 = new Vector(this.startBonePosition.get(0) - 0.05,
+	// this.startBonePosition.get(1),
+	// this.startBonePosition.get(2) + 0.05);
+	// Vector vtop = new Vector(this.endBonePosition.get(0),
+	// this.endBonePosition.get(1), this.endBonePosition.get(2));
+	//
+	// IVertex vertex1 = new Vertex(VectorFactory.createVector3(v1.get(0),
+	// v1.get(1), v1.get(2)));
+	// IVertex vertex2 = new Vertex(VectorFactory.createVector3(v2.get(0),
+	// v2.get(1), v2.get(2)));
+	// IVertex vertex3 = new Vertex(VectorFactory.createVector3(v3.get(0),
+	// v3.get(1), v3.get(2)));
+	// IVertex vertexTop = new Vertex(VectorFactory.createVector3(vtop.get(0),
+	// vtop.get(1), vtop.get(2)));
+	//
+	// this.addVertex(vertexTop);
+	// this.addVertex(vertex1);
+	// this.addVertex(vertex2);
+	// this.addVertex(vertex3);
+	//
+	// this.addTriangle(0, 1, 2);
+	// this.addTriangle(0, 2, 3);
+	// this.addTriangle(0, 1, 3);
+	// this.addTriangle(1, 2, 3);
+	//
+	// // setColorNotSelected();
+	// this.updateRenderStructures();
+	//
+	// }
+
+	private double getBoneRotation(Vector vectorStart, Vector vectorEnd) {
+		vectorStart = new Vector(vectorStart.subtract(vectorStart));
+		vectorEnd = new Vector(vectorEnd.subtract(vectorStart));
+
+		Vector u = vectorEnd.subtract(vectorStart);
+		Vector aufXAchse = vectorStart.add(new Vector(1.0, 0, 0));
+		Vector v = aufXAchse.subtract(vectorStart);
+
+		double skalarProd = u.multiply(v);
+		double lengthU = u.getNorm();
+		double lengthV = v.getNorm();
+
+		double nenner = lengthU * lengthV;
+
+		double value = skalarProd / nenner;
+
+		return Math.acos(value);
 
 	}
 
@@ -314,6 +411,27 @@ public class Bone extends TriangleMesh {
 																						// dieses
 																						// Bones
 		}
+	}
+
+	public Vector rotatePositionByDegree(double winkelDeg, Vector toTurn, Vector drehPunkt) {
+		double winkelRad = degToRad(winkelDeg);
+		double xEnd = toTurn.get(0);
+		double yEnd = toTurn.get(1);
+		double zEnd = toTurn.get(2);
+
+		double xDrehpunkt = drehPunkt.get(0);
+		double yDrehpunkt = drehPunkt.get(1);
+
+		double xStrich = xDrehpunkt + Math.cos(winkelRad) * (xEnd - xDrehpunkt)
+				- Math.sin(winkelRad) * (yEnd - yDrehpunkt);
+		double yStrich = yDrehpunkt + Math.sin(winkelRad) * (xEnd - xDrehpunkt)
+				+ Math.cos(winkelRad) * (yEnd - yDrehpunkt);
+		double zStrich = zEnd;
+
+		Vector returnValue = new Vector(xStrich, yStrich, zStrich);
+
+		return returnValue;
+
 	}
 
 	public void rotateUmDrehpunkt(double winkelDeg, Vector drehPunkt) {
